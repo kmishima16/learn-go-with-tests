0a2e75dceb62d59be2fe48b8e5b0d6e98ee01a6a
三島 <ilfr1616@gmail.com>
2025-10-16 14:14:33 +0000

    refactor: アサーションの共通化

--- hello_test.go
+++ hello_test.go
@@ -3,21 +3,23 @@ package main
 import "testing"
 
 func TestHello(t *testing.T) {
+	assertMessage := func(t testing.TB, got, want string) {
+		t.Helper()
+		if got != want {
+			t.Errorf("got %q want %q", got, want)
+		}
+	}
 	t.Run("あいさつを返す", func(t *testing.T) {
 		got := Hello("World")
 		want := "Hello, World"
 
-		if got != want {
-			t.Errorf("got %q want %q", got, want)
-		}
+		assertMessage(t, got, want)
 	})
 
 	t.Run("挨拶の相手を指定できる", func(t *testing.T) {
 		got := Hello("Chris")
 		want := "Hello, Chris"
 
-		if got != want {
-			t.Errorf("got %q want %q", got, want)
-		}
+		assertMessage(t, got, want)
 	})
 }
