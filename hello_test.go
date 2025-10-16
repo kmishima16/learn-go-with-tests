ba75b60590d544b1c4fba67bbfec923bcee45dbf
三島 <ilfr1616@gmail.com>
2025-10-16 13:43:08 +0000

    update: 名前が空の場合、デフォルト値となる

--- hello_test.go
+++ hello_test.go
@@ -3,10 +3,21 @@ package main
 import "testing"
 
 func TestGreet(t *testing.T) {
-	got := Greet("Chris")
-	want := "Hello, Chris"
+	t.Run("名前を指定して挨拶できる", func(t *testing.T) {
+		got := Greet("Chris")
+		want := "Hello, Chris"
 
-	if got != want {
-		t.Errorf("got %q want %q", got, want)
-	}
+		if got != want {
+			t.Errorf("got %q want %q", got, want)
+		}
+	})
+
+	t.Run("名前が空の場合、デフォルト値を使う", func(t *testing.T) {
+		got := Greet("")
+		want := "Hello, World"
+
+		if got != want {
+			t.Errorf("got %q want %q", got, want)
+		}
+	})
 }
