eba0abb0bfb693343d270e69cb533a52fc337d87
三島 <ilfr1616@gmail.com>
2025-10-16 14:00:18 +0000

    add 挨拶の対象を指定できる

--- hello_test.go
+++ hello_test.go
@@ -3,12 +3,21 @@ package main
 import "testing"
 
 func TestHello(t *testing.T) {
+	t.Run("挨拶の対象（名前）を指定できる", func(t *testing.T) {
+		got := Hello("Chris")
+		want := "Hello, Chris"
+
+		if got != want {
+			t.Errorf("got %q want %q", got, want)
+		}
+	})
 	t.Run("says hello to the world", func(t *testing.T) {
-		got := Hello()
+		got := Hello("World")
 		want := "Hello, World"
 
 		if got != want {
 			t.Errorf("got %q want %q", got, want)
 		}
 	})
+
 }
