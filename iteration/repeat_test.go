75079905c41b08a0cbcd2e923dfa417eebe65e28
三島 <ilfr1616@gmail.com>
2025-10-17 09:56:38 +0000

    add: 5回入力文字列を繰り返す関数

---
+++ iteration/repeat_test.go
@@ -0,0 +1,14 @@
+package iteration
+
+import "testing"
+
+func TestRepeat(t *testing.T) {
+	t.Run("5回繰り返す", func(t *testing.T) {
+		got := Repeat("a")
+		want := "aaaaa"
+
+		if got != want {
+			t.Errorf("got %q want %q", got, want)
+		}
+	})
+}
