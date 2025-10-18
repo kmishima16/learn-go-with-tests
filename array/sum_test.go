64a5e62589cc05a90d9698f8360f94b2f7fa1e1f
三島 <ilfr1616@gmail.com>
2025-10-18 07:06:23 +0000

    add: 配列を合計する関数Sumの実装

---
+++ array/sum_test.go
@@ -0,0 +1,16 @@
+package array
+
+import "testing"
+
+func TestSum(t *testing.T) {
+	t.Run("配列の合計を計算する", func(t *testing.T) {
+		numbers := [5]int{1, 2, 3, 4, 5}
+
+		got := Sum(numbers[:])
+		want := 15
+
+		if got != want {
+			t.Errorf("got %v want %v", got, want)
+		}
+	})
+}
