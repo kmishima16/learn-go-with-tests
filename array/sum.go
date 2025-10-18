ba93eb3e8b3c4ad27a34da43c3789027a0ef1368
三島 <ilfr1616@gmail.com>
2025-10-18 07:19:22 +0000

    add: 配列の末尾を合計する関数SumAllTailsを追加

--- array/sum.go
+++ array/sum.go
@@ -15,3 +15,16 @@ func SumAll(slices ...[]int) []int {
 	}
 	return sums
 }
+
+func SumAllTails(slices ...[]int) []int {
+	var sums []int
+	for _, slice := range slices {
+		if len(slice) == 0 {
+			sums = append(sums, 0)
+		} else {
+			tail := slice[1:]
+			sums = append(sums, Sum(tail))
+		}
+	}
+	return sums
+}
--- array/sum_test.go
+++ array/sum_test.go
@@ -4,12 +4,26 @@ import (
 	"reflect"
 	"testing"
 )
+func TestSumAllTails(t *testing.T) {
+	t.Run("複数のスライスの最後の要素以外の合計を計算する", func(t *testing.T) {
+		
+		got := SumAllTails([]int{1, 2}, []int{3, 4})
+		want := []int{2, 4}
+
+		if !reflect.DeepEqual(got, want) {
+			t.Errorf("got %v want %v", got, want)
+		}
+
+	})
+}
+
+
 
 func TestSumAll(t *testing.T) {
 	t.Run("複数のスライスの合計を計算する", func(t *testing.T) {
 
-		got := SumAll([]int{1, 2}, []int{3, 4})
-		want := []int{3, 7}
+		got := SumAll([]int{1, 2}, []int{3, 4, 5})
+		want := []int{3, 12}
 
 		if !reflect.DeepEqual(got, want) {
 			t.Errorf("got %v want %v", got, want)
