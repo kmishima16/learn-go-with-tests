06435e30a8f7d5c6bb18dcd54ad98a508550cb6d
三島 <ilfr1616@gmail.com>
2025-10-18 07:07:44 +0000

    test: スライスでの合計チェックを追加

--- array/sum_test.go
+++ array/sum_test.go
@@ -13,4 +13,15 @@ func TestSum(t *testing.T) {
 			t.Errorf("got %v want %v", got, want)
 		}
 	})
+
+	t.Run("スライスの合計を計算する", func(t *testing.T) {
+		numbers := []int{1, 2, 3, 4, 5}
+
+		got := Sum(numbers)
+		want := 15
+
+		if got != want {
+			t.Errorf("got %v want %v", got, want)
+		}
+	})
 }
