0500b90d0d8e36b3a164cb840193ba8c338b1bb0
三島 <ilfr1616@gmail.com>
2025-10-23 11:52:06 +0000

    add: Triangle構造体と面積計算メソッドを追加

--- structs/shapes_test.go
+++ structs/shapes_test.go
@@ -23,6 +23,7 @@ func TestArea(t *testing.T) {
 		{shape: Rectangle{Width: 12, Height: 6}, want: 72.0},
 		{shape: Circle{Radius: 10}, want: 314.1592653589793},
 		{shape: Circle{Radius: 1}, want: 3.141592653589793},
+		{shape: Triangle{Base: 12, Height: 6}, want: 36.0},
 	}
 
 	for _, tt := range areaTests {
