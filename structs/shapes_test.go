1317f1e723fde4ff5568b5caa37813ccf9aa1226
三島 <ilfr1616@gmail.com>
2025-10-23 11:49:44 +0000

    refactor: Shapeインターフェースを追加しテストを簡素化

--- structs/shapes_test.go
+++ structs/shapes_test.go
@@ -15,23 +15,22 @@ func TestPerimeter(t *testing.T) {
 }
 
 func TestArea(t *testing.T) {
-	t.Run("長方形の面積", func(t *testing.T) {
-		rect := Rectangle{Width: 10.0, Height: 10.0}
-		got := rect.Area()
-		want := 100.0
 
+	checkArea := func(t testing.TB, shape Shape, want float64) {
+		t.Helper()
+		got := shape.Area()
 		if got != want {
 			t.Errorf("got %.2f want %.2f", got, want)
 		}
+	}
+
+	t.Run("長方形の面積", func(t *testing.T) {
+		rect := Rectangle{Width: 10.0, Height: 10.0}
+		checkArea(t, rect, 100.0)
 	})
 
 	t.Run("円の面積", func(t *testing.T) {
 		circle := Circle{Radius: 10.0}
-		got := circle.Area()
-		want := 314.1592653589793
-
-		if got != want {
-			t.Errorf("got %.2f want %.2f", got, want)
-		}
+		checkArea(t, circle, 314.1592653589793)
 	})
 }
