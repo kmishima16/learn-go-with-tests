323dea9729448add575b1c6750d57353ed793f71
三島 <ilfr1616@gmail.com>
2025-10-23 11:41:43 +0000

    add: 長方形の面積を求める関数

--- structs/shapes_test.go
+++ structs/shapes_test.go
@@ -12,3 +12,14 @@ func TestPerimeter(t *testing.T) {
 		}
 	})
 }
+
+func TestArea(t *testing.T) {
+	t.Run("長方形の面積", func(t *testing.T) {
+		got := Area(10.0, 10.0)
+		want := 100.0
+
+		if got != want {
+			t.Errorf("got %.2f want %.2f", got, want)
+		}
+	})
+}
