d062e97caac9705388052440282eff69dd13eb0b
三島 <ilfr1616@gmail.com>
2025-10-23 11:41:06 +0000

    add: 長方形の周囲の長さを求める関数

---
+++ structs/shapes_test.go
@@ -0,0 +1,14 @@
+package structs
+
+import "testing"
+
+func TestPerimeter(t *testing.T) {
+	t.Run("長方形の周囲の長さ", func(t *testing.T) {
+		got := Perimeter(10.0, 10.0)
+		want := 40.0
+
+		if got != want {
+			t.Errorf("got %.2f want %.2f", got, want)
+		}
+	})
+}
